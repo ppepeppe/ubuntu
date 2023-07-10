28ae5b4014b637bcd7aa4f6527e4cfdb55a0398c
byunseongdo <dmdsns@naver.com>
2023-07-11 01:40:29 +0900

    springboot test

---
+++ pratice/pratice/src/test/java/com/ktds/pratice/PraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktds.pratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
